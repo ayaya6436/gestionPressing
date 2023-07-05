209a3928caab763026cd83b225657cfc37ce748d
ayaya <ayaya6436@gmail.com>
2023-07-05 15:30:02 +0000

    mise a jour dashboard

--- dashboard.php
+++ dashboard.php
@@ -27,38 +27,38 @@
 				<hr>
 				<div class="row">
 				<div class="alert alert-success col-md-3 ml-4">
-					<p><b><large>Benefice du jour</large></b></p>
+					<p><b><large>Benefice </large></b></p>
 				<hr>
 					<p class="text-right"><b><large><?php 
 					include 'db_connect.php';
-					$commande = $conn->query("SELECT SUM(montant_total) as montant FROM commandes where pay_status= 1 and date(date_creation)= '".date('Y-m-d')."'");
-					echo $commande->num_rows > 0 ? number_format($commande->fetch_array()['montant'],2) : "0.00";
-					
+					$commande = $conn->query("SELECT SUM(montant_total) as montant FROM commandes WHERE pay_status = 1 AND DATE(date_creation) = '" . date('Y-m-d') . "'");
+					$montant = $commande->num_rows > 0 ? $commande->fetch_array()['montant'] : 0;
+					echo $montant !== null ? number_format($montant, 2) : "Fcfa";
 
 					 ?></large></b></p>
 					
 				</div>
 				<div class="alert alert-info col-md-3 ml-4">
-					<p><b><large>Total Client du jour</large></b></p>
+					<p><b><large>Total Client</large></b></p>
 				<hr>
 					<p class="text-right"><b><large><?php 
 					include 'db_connect.php';
-					// $laundry = $conn->query("SELECT count(id) as `count` FROM commandes where  date(date_created)= '".date('Y-m-d')."'");
-					// echo $laundry->num_rows > 0 ? number_format($laundry->fetch_array()['count']) : "0";
+					$commande = $conn->query("SELECT count(id) as `count` FROM commandes where  date(date_creation)= '".date('Y-m-d')."'");
+					echo $commande->num_rows > 0 ? number_format($commande->fetch_array()['count'])." client(es)" : "0";
 
 					 ?></large></b></p>
-					 4 clients
+					 
 				</div>
 				<div class="alert alert-primary col-md-3 ml-4">
-					<p><b><large>Total article nettoye du jour</large></b></p>
+					<p><b><large>Total article nettoye </large></b></p>
 				<hr>
 					<p class="text-right"><b><large><?php 
 					include 'db_connect.php';
-					// $laundry = $conn->query("SELECT count(id) as `count` FROM commandes where status = 3 and date(date_created)= '".date('Y-m-d')."'");
-					// echo $laundry->num_rows > 0 ? number_format($laundry->fetch_array()['count']) : "0";
+					$commande = $conn->query("SELECT count(id) as `count` FROM commandes where status = 3 and date(date_creation)= '".date('Y-m-d')."'");
+					echo $commande->num_rows > 0 ? number_format($commande->fetch_array()['count']). " articles" : "0";
 
 					 ?></large></b></p>
-					 2 articles
+					 
 				</div>
 				</div>
 			</div>
